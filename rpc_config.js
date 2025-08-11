2cd7abe3e6826fb746c52fa24f808b83461bee25
Srajavath <shireshar1@gmail.com>
2025-08-11 13:58:29 +0530

    Initial commit

--- rpc_config.js
+++ rpc_config.js
@@ -6,11 +6,6 @@ import {
   deleteStudent
 } from "./rpc_implementation.js";
 
-import { getGoogleTrends, getGoogleTrendsForMultipleKeywords } from './controllers/googleTrendsController.js';
-
-
-
-
 const rpc = new RetoolRPC({
   apiToken: 'retool_01k0evfddn0begk04435phht6t',
   host: 'https://zetaglobalcustomerengineeringintern.retool.com',  
@@ -57,44 +52,6 @@ rpc.register({
     id: { type: 'number', required: true },
   },
   implementation: deleteStudent,
-});
-
-
-
-rpc.register({
-  name: 'getGoogleTrends',  
-  arguments: {
-    keyword: { type: 'string', required: true },
-    startTime: { type: 'string', required: false },
-    endTime: { type: 'string', required: false },
-  },
-  implementation: async (args, context) => {
-    const { keyword, startTime, endTime } = args;
-
-    const start = startTime ? new Date(startTime) : undefined;
-    const end = endTime ? new Date(endTime) : undefined;
-    
-
-    return await getGoogleTrends(keyword, start, end);
-  }
-});
-
-rpc.register({
-  name: 'getGoogleTrendsMultiple', 
-  arguments: {
-    keywords: { type: 'string', required: true }, 
-    startTime: { type: 'string', required: false },
-    endTime: { type: 'string', required: false },
-  },
-  implementation: async (args, context) => {
-    const { keywords, startTime, endTime } = args;
-
-    const keywordArray = keywords.split(',').map(k => k.trim());
-    const start = startTime ? new Date(startTime) : undefined;
-    const end = endTime ? new Date(endTime) : undefined;
-
-    return await getGoogleTrendsForMultipleKeywords(keywordArray, start, end);
-  }
 });
 
 rpc.listen();
